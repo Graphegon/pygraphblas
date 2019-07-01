4a1f844f79b4723bd5f91e8aa67645f94c974179
Michel Pelletier <pelletier.michel@gmail.com>
2019-07-01 09:38:39 -0700

    added temp decl of to_dense until PR fixing it is merged.

--- pygraphblas/build.py
+++ pygraphblas/build.py
@@ -3,7 +3,17 @@ ffibuilder = FFI()
 
 ffibuilder.set_source(
     "_pygraphblas",
-    r"""#include "LAGraph.h" """,
+    r"""#include "LAGraph.h" 
+
+    // missing from LAGraph.h for the moment
+    GrB_Info LAGraph_Vector_to_dense
+    (
+        GrB_Vector *vdense,     // output vector
+        GrB_Vector v,           // input vector
+        void *id                // pointer to value to fill vdense with
+     ) ;
+
+    """,
     libraries=['graphblas', 'lagraph'])
 
 gb_cdef = open('pygraphblas/cdef/gb_2.3.3_cdef.h')
