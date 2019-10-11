ddd5a1f2c2fd279e7d2b5efc7bbe5f6b955de5d3
Michel Pelletier <pelletier.michel@gmail.com>
2019-10-10 19:45:03 -0700

    more dnn tweaks.

--- dnn.sh
+++ dnn.sh
@@ -1,22 +1,42 @@
-mkdir -p dnn_demo && cd dnn_demo
+download () {
+    if [ ! -f "sparse-images-$2.tsv" ]; then
+        wget https://graphchallenge.s3.amazonaws.com/synthetic/sparsechallenge_2019/mnist/sparse-images-$2.tsv.gz
+        gunzip sparse-images-$2.tsv.gz
+        rm sparse-images-$2.tsv.gz
+    fi
 
-if [ ! -f "sparse-images-1024.tsv" ]; then
-    wget https://graphchallenge.s3.amazonaws.com/synthetic/sparsechallenge_2019/mnist/sparse-images-1024.tsv.gz
-    gunzip sparse-images-1024.tsv.gz
-    rm sparse-images-1024.tsv.gz
+    if [ ! -f "neuron$2-l$3-categories.tsv" ]; then
+        wget https://graphchallenge.s3.amazonaws.com/synthetic/sparsechallenge_2019/dnn/neuron$2-l$3-categories.tsv
+    fi
+
+    if [ ! -d "neuron$2" ]; then
+        wget https://graphchallenge.s3.amazonaws.com/synthetic/sparsechallenge_2019/dnn/neuron$2.tar.gz
+        zcat neuron$2.tar.gz | tar xf -
+        rm neuron$2.tar.gz
+    fi
+}
+
+mkdir -p $1 && cd $1
+
+neurons=( 1024 4096 16384 65536 )
+nlayers=( 120 480 1920 )
+
+if [ $# -eq 1 ]; then
+    for i in "${neurons[@]}"
+    do
+        for j in "${nlayers[@]}"
+        do
+            download $1 $i $j
+        done
+    done
+else
+    download $1 $2 $3
 fi
 
-if [ ! -f "neuron1024-l120-categories.tsv" ]; then
-    wget https://graphchallenge.s3.amazonaws.com/synthetic/sparsechallenge_2019/dnn/neuron1024-l120-categories.tsv
-fi
+cd -
 
-if [ ! -d "neuron1024" ]; then
-    wget https://graphchallenge.s3.amazonaws.com/synthetic/sparsechallenge_2019/dnn/neuron1024.tar.gz
-    zcat neuron1024.tar.gz | tar xvf -
-    rm neuron1024.tar.gz
-fi
-
-cd ..
-
-docker run -v `pwd`/dnn_demo:/pygraphblas/dnn_demo -v `pwd`/pygraphblas:/pygraphblas/pygraphblas -it pygraphblas/pygraphblas ipython -i -m pygraphblas.demo.dnn
+docker run --env DEST=$1 --env NEURONS=$2 --env NLAYERS=$3 \
+       -v $1:$1 \
+       -v `pwd`/pygraphblas:/pygraphblas/pygraphblas \
+       -it pygraphblas/pygraphblas ipython -i -m pygraphblas.demo.dnn
 
--- pygraphblas/demo/dnn.py
+++ pygraphblas/demo/dnn.py
@@ -1,91 +1,108 @@
-import gzip
-from functools import wraps
+import os
+from functools import wraps, partial
 from time import time
+from statistics import mean
 from pathlib import Path
-from pygraphblas import Matrix, lib
+from pygraphblas import Matrix, Vector, lib
 from pygraphblas.semiring import plus_times_fp32, plus_plus_fp32
 from multiprocessing.pool import ThreadPool
 from multiprocessing import cpu_count
 
 
-nfeatures = 60000
-nneurons = 1024
+NFEATURES = 60000
+BIAS = {1024: -0.3, 4096: -0.35, 16384: -0.4, 65536: -0.45}
 
 def timing(f):
     @wraps(f)
     def wrap(*args, **kw):
-        print('timing %r ...' % f.__name__)
         ts = time()
         result = f(*args, **kw)
         te = time()
-        print('func:%r took: %2.4f sec' % (f.__name__, te-ts))
+        print('func:%r ran %sx took: %2.4f avg' % (f.__name__, te-ts))
         return result
     return wrap
 
 @timing
 def dnn(W, Bias, Y0):
-    Y = Matrix.from_type(Y0.gb_type, nfeatures, nneurons)
-    for layer, (w, b) in enumerate(zip(W, Bias)):
-        with plus_times_fp32:
-            (Y0 if layer == 0 else Y).mxm(w, out=Y)
+    Y = Y0
+    for w, b in zip(W, Bias):
+        Y = Y @ w
         with plus_plus_fp32:
-            Y.mxm(b, out=Y)
-        Y.select(lib.GxB_GT_ZERO, out=Y) # Y = Y > 0
-        M = Y > 32
-        if len(M):
-            Y[M] = 32
-    return Y
-
-@timing
-def dnn2(W, Bias, Y):
-    Y = Matrix.from_type(Y0.gb_type, nfeatures, nneurons)
-    for layer, (w, b) in enumerate(zip(W, Bias)):
-        Y = Y.mxm((Y @ w), b, semiring=plus_plus_fp32)
+            Y = Y @ b
         Y = Y > 0
         M = Y > 32
-        if len(M):
+        if M:
             Y[M] = 32
     return Y
 
 @timing
-def load_images():
-    images = Path('./dnn_demo/sparse-images-1024.tsv')
+def load_images(neurons, dest):
+    images = Path('{}/sparse-images-{}.tsv'.format(dest, neurons))
     with images.open() as i:
-        return Matrix.from_tsv(i, lib.GrB_FP32, nfeatures, nneurons)
+        return Matrix.from_tsv(i, lib.GrB_FP32, NFEATURES, neurons)
 
-@timing
-def load_categories():
-    cats = Path('./dnn_demo/neuron1024-l120-categories.tsv')
-    result = Vector.from_type(bool, nfeatures)
+def load_categories(neurons, nlayers, dest):
+    cats = Path('{}/neuron{}-l{}-categories.tsv'.format(dest, neurons, nlayers))
+    result = Vector.from_type(bool, NFEATURES)
     with cats.open() as i:
         for line in i.readlines():
-            result[int(line.strip())] = True
+            result[int(line.strip())-1] = True
     return result
 
-def load_layer(i):
-    l = Path('./dnn_demo/neuron1024/n1024-l{}.tsv'.format(str(i+1)))
+def load_layer(i, dest):
+    l = Path('{}/neuron{}/n{}-l{}.tsv'.format(dest, neurons, neurons, str(i+1)))
     with l.open() as f:
-        return Matrix.from_tsv(f, lib.GrB_FP32, nneurons, nneurons)
+        return Matrix.from_tsv(f, lib.GrB_FP32, neurons, neurons)
 
 @timing
-def generate_layers(layers=120):
-    result = []
-    neurons = Path('./dnn_demo/neuron1024')
+def generate_layers(neurons, nlayers, dest):
+    neurons = Path('{}/neuron{}'.format(dest, neurons))
     with ThreadPool(cpu_count()) as pool:
-        return pool.map(load_layer, range(layers))
+        return pool.map(partial(load_layer, dest=dest), range(nlayers))
 
 @timing
-def generate_bias(layers=120):
+def generate_bias(neurons, nlayers):
     result = []
-    for i in range(layers):
-        bias = Matrix.from_type(lib.GrB_FP32, nneurons, nneurons)
-        for i in range(nneurons):
-            bias[i,i] = 0.3
+    for i in range(nlayers):
+        bias = Matrix.from_type(lib.GrB_FP32, neurons, neurons)
+        for i in range(neurons):
+            bias[i,i] = BIAS[neurons]
         bias.nvals # causes async completion
         result.append(bias)
     return result
 
+@timing
+def run(neurons, images, layers, bias, dest):
+    result = dnn(layers,
+                 bias,
+                 images)
+    r = result.reduce_vector()
+    cats = r.apply(lib.GxB_ONE_BOOL, out=Vector.from_type(bool, r.size))
+    truecats = load_categories(neurons, nlayers, dest)
+    assert cats == truecats
+
+
+num_neurons = [1024, 4096, 16384, 65536]
+num_layers = [120, 480, 1920]
 
 if __name__ == '__main__':
-    result = dnn(generate_layers(), generate_bias(), load_images())
-    cat = load_categories()
+    dest = os.getenv('DEST')
+    neurons = os.getenv('NEURONS')
+    nlayers = os.getenv('NLAYERS')
+
+    if neurons and nlayers:
+        neurons = int(neurons)
+        nlayers = int(nlayers)
+        images = load_images(neurons, dest)
+        layers = generate_layers(neurons, nlayers, dest)
+        bias = generate_bias(neurons, nlayers)
+        run(neurons, images, layers, bias, dest)
+    else:
+        for neurons in num_neurons:
+            print('Building layers for %s neurons' % neurons)
+            layers = generate_layers(neurons, 1920, dest)
+            bias = generate_bias(neurons, 1920)
+            images = load_images(neurons, dest)
+            for nlayers in num_layers:
+                print('Benching %s neurons %s layers' % (neurons, nlayers))
+                run(neurons, images, layers[:nlayers], bias[:nlayers], dest)
