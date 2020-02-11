019a9e12a827c0ee7793dd61df57393f371e92a0
Michel Pelletier <pelletier.michel@gmail.com>
2020-02-11 08:17:50 -0800

    make travis way faster.

--- test.sh
+++ test.sh
@@ -1,1 +1,5 @@
+if [ -n "$1" ]
+  then
+      docker pull graphblas/pygraphblas-notebook:latest
+fi
 docker run -v `pwd`/tests:/home/jovyan/tests -v `pwd`/pygraphblas:/home/jovyan/pygraphblas -it graphblas/pygraphblas-notebook pytest --cov=pygraphblas --cov-report=term-missing
