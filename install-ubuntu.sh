50901d171d78bbda32cc64c9ccb4b571bffde12c
Gabor Szarnyas <szarnyasg@gmail.com>
2020-03-30 14:37:50 +0200

    Update install-ubuntu.sh script to new SS:GrB

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
 # Use this script to install pygraphblas on Ubuntu 18.04
+set -e
 
 # Install the dependencies for building the C libraries
 sudo apt install -y curl m4 g++
 
-# Install the required versions of SuiteSparse:GraphBLAS and LAGraph
-export JOBS=$(nproc) # use all threads for compiling
-curl -s -L  http://faculty.cse.tamu.edu/davis/GraphBLAS/GraphBLAS-3.1.1.tar.gz | tar -xz && \
-    cd GraphBLAS-3.1.1 && \
-    make library && \
-    sudo make install && \
-    sudo ldconfig && \
-    cd ..
-git clone --branch 22July2019 https://github.com/GraphBLAS/LAGraph.git && \
-    cd LAGraph && \
-    make library && \
-    sudo make install && \
-    sudo ldconfig && \
-    cd ..
+# Install the required version of SuiteSparse:GraphBLAS
+
+SS_RELEASE=v3.2.0
+SS_BURBLE=0
+JOBS=$(nproc) # use all threads for compiling
+
+git clone  --branch ${SS_RELEASE} --single-branch https://github.com/DrTimothyAldenDavis/GraphBLAS.git
+cd GraphBLAS
+make library CFLAGS=-DGB_BURBLE=${SS_BURBLE}
+sudo make install
+cd ..
 
 # Install Python components
 sudo apt install -y python3-pip
-pip3 install setuptools pytest pytest-cov ipdb RISE graphviz numba contextvars colorama
+pip3 install -r notebook-requirements.txt
 
 # Install pygraphblas as a user (does not require root privileges)
 python3 setup.py install --user
