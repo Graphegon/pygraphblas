1246fb7372f9411ca5135dd2e8bae376d5b10c7d
Márton Elekes <marci543@users.noreply.github.com>
2020-04-01 18:14:36 +0200

    Add --rm option to docker run commands, improve build (#32)
    * Remove duplicate key from .travis.yml
    
    * Add --rm option to docker run commands

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@ if [ -n "$1" ]
   then
       docker pull graphblas/pygraphblas-notebook:latest
 fi
-docker run -v `pwd`/tests:/home/jovyan/tests -v `pwd`/pygraphblas:/home/jovyan/pygraphblas -it graphblas/pygraphblas-notebook pytest --cov=pygraphblas --cov-report=term-missing
+docker run --rm -v `pwd`/tests:/home/jovyan/tests -v `pwd`/pygraphblas:/home/jovyan/pygraphblas -it graphblas/pygraphblas-notebook pytest --cov=pygraphblas --cov-report=term-missing
