4ac0fd2f3f251bddbf5a2c582d0ef4150548cce4
Gabor Szarnyas <szarnyasg@gmail.com>
2020-04-11 12:57:50 +0200

    Add usage instructions to build script

--- build.sh
+++ build.sh
@@ -1,2 +1,10 @@
+if [ $# -eq 0 ]
+    then
+        echo "Usage: ./build.sh SS_RELEASE"
+        echo
+        echo "Example: ./build.sh v3.2.0"
+        exit 1
+fi
+
 docker build --build-arg SS_RELEASE=$1 -t graphblas/pygraphblas-notebook:$1 .
 docker tag graphblas/pygraphblas-notebook:$1 graphblas/pygraphblas-notebook:latest
