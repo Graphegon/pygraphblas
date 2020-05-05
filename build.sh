1ccee35de3fe2752e0a6ed670b13183b738f35d1
Michel Pelletier <pelletier.michel@gmail.com>
2020-05-05 11:27:42 -0700

    minimal and notebook builds.

--- build.sh
+++ build.sh
@@ -1,17 +1,21 @@
 if [ $# -eq 0 ]
     then
-        echo "Usage: ./build.sh SS_RELEASE"
+        echo "Usage: ./build.sh SS_RELEASE BASE_NAME BRANCH"
         echo
-        echo "Example: ./build.sh v3.2.0"
+        echo "Example: ./build.sh v3.2.0 minimal master"
         exit 1
 fi
+
+BASE_NAME=minimal
+BRANCH=master
 
 /bin/rm -Rf docker_build
 mkdir docker_build
 pushd docker_build
-git clone https://github.com/michelp/pygraphblas.git
+git clone --branch $BRANCH https://github.com/michelp/pygraphblas.git
 cd pygraphblas
-docker build --build-arg SS_RELEASE=$1 -t graphblas/pygraphblas-notebook:$1 .
-docker tag graphblas/pygraphblas-notebook:$1 graphblas/pygraphblas-notebook:latest
-docker push graphblas/pygraphblas-notebook:latest
+docker build -f Dockerfile-${BASE_NAME} --build-arg SS_RELEASE=$1 -t graphblas/pygraphblas-${BASE_NAME}:$1 .
+docker push graphblas/pygraphblas-${BASE_NAME}:$1
+docker tag graphblas/pygraphblas-${BASE_NAME}:$1 graphblas/pygraphblas-${BASE_NAME}:latest
+docker push graphblas/pygraphblas-${BASE_NAME}:latest
 
