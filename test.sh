687068cd4e5957ab710663eef427932f82aeb7c2
marci543 <marci543@users.noreply.github.com>
2020-06-04 01:02:20 +0200

    Build and test SuiteSparse from code instead of package

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 if [ -n "$1" ]
   then
-      docker pull graphblas/pygraphblas-minimal:latest
+      docker pull graphblas/pygraphblas-notebook:latest
 fi
 docker run --rm \
        -v `pwd`/tests:/home/jovyan/tests \
        -v `pwd`/pygraphblas:/home/jovyan/pygraphblas \
-       -it graphblas/pygraphblas-minimal \
+       -it graphblas/pygraphblas-notebook \
        pytest --cov=pygraphblas --cov-report=term-missing
