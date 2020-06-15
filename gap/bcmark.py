d64e0920e339235cf41f6f2f773d17723f72632f
Michel Pelletier <pelletier.michel@gmail.com>
2020-06-15 08:45:15 -0700

    add gap benchmarks.

---
+++ gap/bcmark.py
@@ -0,0 +1,92 @@
+import sys
+from pathlib import Path
+from time import time
+from statistics import mean
+
+from pygraphblas import *
+
+def load_sources(subdir):
+    fname = '/GAP/GAP-{0}/GAP-{0}_sources.mtx'.format(subdir)
+    if not Path(fname).exists():
+        raise Exception('No sourcefile for {} found at {}'.format(subdir, fname))
+    with open(fname) as f:
+        m = Matrix.from_mm(f, UINT64)
+    return list(zip(*(iter([i[2]-1 for i in m]),) * 4))
+
+def bc(sources, AT, A):
+    n = A.nrows
+    ns = len(sources)
+    centrality = Vector.dense (FP32, n, 0)
+    paths      = Matrix.dense (FP32, ns, n, 0)
+    frontier   = Matrix.sparse(FP32, ns, n)
+    S = []
+    
+    for i, s in enumerate(sources):
+        paths[   i, sources[i]] = 1
+        frontier[i, sources[i]] = 1
+
+    frontier.mxm(
+        A, 
+        out=frontier, 
+        mask=paths, 
+        semiring=FP32.PLUS_FIRST, 
+        desc=descriptor.oocr)
+    
+    for depth in range(n):
+        if frontier.nvals == 0:
+            break
+        s = Matrix.sparse(BOOL, ns, n)
+        frontier.apply(BOOL.ONE, out=s)
+        S.append(s)
+
+        with Accum(FP32.PLUS):
+            paths[:,:] = frontier
+
+        frontier.mxm(
+            A, 
+            out=frontier, 
+            mask=paths, 
+            semiring=FP32.PLUS_FIRST, 
+            desc=descriptor.oocr)
+      
+    bc = Matrix.dense(FP32, ns, n, 1)
+    W = Matrix.sparse(FP32, ns, n)
+    
+    for i in range(depth - 1, 0, -1):
+        bc.emult(paths, FP32.DIV, out=W, mask=S[i], desc=Replace)
+        W.mxm(AT, out=W, mask=S[i-1], semiring=FP32.PLUS_FIRST, desc=Replace)
+        W.emult(paths, FP32.TIMES, accum=FP32.PLUS, out=bc)
+    
+    centrality[:] = -ns
+    bc.reduce_vector(accum=FP32.PLUS, out=centrality, desc=TransposeA)
+    
+    return centrality
+
+if __name__ == '__main__':
+    argc = len(sys.argv)
+    threads = int(sys.argv[1]) if argc > 1 else None
+
+    if threads is not None:
+        options_set(nthreads=threads)
+
+    for subdir in ['kron', 'road', 'twitter', 'urand', 'web']:
+        fname = '/GAP/GAP-{0}/GAP-{0}.grb'.format(subdir)
+        if not Path(fname).exists():
+            print('Skipping {} No binfile found at {}'.format(subdir, fname))
+            continue
+        
+        print('loading {} file.'.format(fname))
+        M = Matrix.from_binfile(fname.encode('utf8'))
+
+        print('Betweening...')
+
+        timings = []
+        sources = load_sources(subdir)
+        for i, s in enumerate(sources):
+            start = time()
+            pr = bc(s, M.T, M)
+            delta = time() - start
+            print('Round {} took {}'.format(i, delta))
+            timings.append(delta)
+
+        print('BetweenessCentraility {} average time {} for {} rounds'.format(subdir, mean(timings), rounds))
---
+++ gap/prmark.py
@@ -0,0 +1,64 @@
+import sys
+from pathlib import Path
+from time import time
+from statistics import mean
+
+from pygraphblas import *
+
+def pagerank(A, damping, itermax):
+    n = A.nrows
+    r = Vector.sparse(FP32, n)
+    t = Vector.sparse(FP32, n)
+    d = A.reduce_vector()
+    with Accum(FP32.DIV):
+        d[:] = damping
+    r[:] = 1.0 / n
+    teleport = (1 - damping) / n
+    tol = 1e-4
+    rdiff = 1.0
+    for i in range(itermax):
+        if rdiff <= tol:
+            break
+        temp = t ; t = r ; r = temp
+        w = t / d
+        r[:] = teleport
+        A.mxv(w,
+              out=r,
+              accum=FP32.PLUS,
+              semiring=FP32.PLUS_SECOND,
+              desc=TransposeA)
+        t -= r
+        t = abs(t)
+        rdiff = t.reduce_float()
+    print('PR took {} iterations'.format(i))
+    return r
+
+argc = len(sys.argv)
+
+rounds = int(sys.argv[1]) if argc > 1 else 16
+threads = int(sys.argv[2]) if argc > 2 else None
+
+if threads is not None:
+    options_set(nthreads=threads)
+
+if __name__ == '__main__':
+    for subdir in ['kron', 'road', 'twitter', 'urand', 'web']:
+        fname = '/GAP/GAP-{}/GAP-{}.grb'.format(subdir, subdir)
+        if not Path(fname).exists():
+            print('Skipping {} No binfile found at {}'.format(subdir, fname))
+            continue
+        
+        print('loading {} file.'.format(fname))
+        M = Matrix.from_binfile(fname.encode('utf8'))
+
+        print('Ranking...')
+
+        timings = []
+        for i in range(rounds):
+            start = time()
+            pr = pagerank(M, 0.85, 100)
+            delta = time() - start
+            print('Round {} took {}'.format(i, delta))
+            timings.append(delta)
+
+        print('PageRank {} average time {} for {} rounds'.format(subdir, mean(timings), rounds))
