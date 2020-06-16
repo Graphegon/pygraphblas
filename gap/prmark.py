62fdd6bb5becf7cf46c383842d80ca33273f37b6
Michel Pelletier <pelletier.michel@gmail.com>
2020-06-15 18:56:25 -0700

    page rank fixes.

--- gap/prmark.py
+++ gap/prmark.py
@@ -5,13 +5,11 @@ from statistics import mean
 
 from pygraphblas import *
 
-def pagerank(A, damping, itermax):
+def pagerank(A, d, damping, itermax):
     n = A.nrows
     r = Vector.sparse(FP32, n)
     t = Vector.sparse(FP32, n)
-    d = A.reduce_vector()
-    with Accum(FP32.DIV):
-        d[:] = damping
+    d.assign_scalar(damping, accum=FP32.DIV)
     r[:] = 1.0 / n
     teleport = (1 - damping) / n
     tol = 1e-4
@@ -28,10 +26,9 @@ def pagerank(A, damping, itermax):
               semiring=FP32.PLUS_SECOND,
               desc=TransposeA)
         t -= r
-        t = abs(t)
+        t.apply(FP32.ABS, out=t)
         rdiff = t.reduce_float()
-        print('rdiff {}'.format(rdiff))
-    print('PR took {} iterations'.format(i))
+        print('{}: {:.6f}'.format(i, rdiff))
     return r
 
 argc = len(sys.argv)
@@ -44,25 +41,47 @@ if threads is not None:
 
 if __name__ == '__main__':
     for subdir in ['road', 'kron', 'twitter', 'urand', 'web']:
-        fname = 'GAP/GAP-{}/GAP-{}.grb'.format(subdir, subdir)
+        fname = '/GAP/GAP-{}/GAP-{}.grb'.format(subdir, subdir)
         if not Path(fname).exists():
             print('Skipping {} No binfile found at {}'.format(subdir, fname))
             continue
-        
-        print('loading {} file.'.format(fname))
-        M = Matrix.from_binfile(fname.encode('utf8'))
 
-        print('Ranking...')
+        print('loading {} file.'.format(fname))
+        A = Matrix.from_binfile(fname.encode('utf8'))
+        M = A.pattern(UINT64)
+        M.options_set(format=lib.GxB_BY_COL)
+        M.nvals  # finish compute
+        d_out = M.reduce_vector()
+        d_in =  M.reduce_vector(desc=TransposeA)
+        edges_added = 0
+        if d_out.nvals < M.nrows or d_in.nvals < M.nrows:
+            printf ("Matrix has {} empty rows and {} empty cols\n".format(M.nrows - d_out.nvals, M.nrows - d_in.nvals))
+            for i in range(M.nrows):
+                din = d_in[i]
+                dout = d_in[i]
+                if din is False or dout is False:
+                    edges_added += 1
+                    M[i:i] = True
+
+        d_out = Vector.sparse(FP32, A.nrows)
+        M.reduce_vector(FP32.PLUS_MONOID, out=d_out)
+        M = M.pattern()
+
+        print ("input graph: nodes: {} edges: {}".format(M.nrows, M.nvals))
+        print ("diag entries added: {}".format(edges_added))
+
+        print ('Ranking...')
 
         timings = []
         for i in range(rounds):
             start = time()
-            result = pagerank(M, 0.85, 100)
+            result = pagerank(M, d_out, 0.85, 100)
             delta = time() - start
             print('Round {} took {}'.format(i, delta))
             timings.append(delta)
-            resultm = Matrix.sparse(result.type, result.size, 1)
-            resultm[:,0] = result
-            resultm.to_mm(open('pr_{}_{}.mtx'.format(subdir, i), 'a'))
+            # uncomment to dump binary result files:
+            # resultm = Matrix.sparse(result.type, result.size, 1)
+            # resultm[:,0] = result
+            # resultm.to_mm(open('pr_{}_{}.mtx'.format(subdir, i), 'a'))
 
         print('PageRank {} average time {} for {} rounds'.format(subdir, mean(timings), rounds))
