7c4309917456d2aeabbdbd8660562b593fd24018
Michel Pelletier <pelletier.michel@gmail.com>
2021-01-15 19:59:11 -0800

    remove docs from this branch.

--- pdoc.sh
+++ pdoc.sh
@@ -1,4 +1,5 @@
-BASE_NAME="notebook"
-BASE_PATH="/home/jovyan"
-
-docker run --rm -v `pwd`:/home/joyvan/pygraphblas -it graphblas/pygraphblas-notebook:latest pdoc --html -f -o /home/joyvan/pygraphblas/docs pygraphblas/
+git checkout gh-pages
+docker run --rm -v `pwd`:/home/joyvan/pygraphblas -it graphblas/pygraphblas-notebook:latest pdoc --html -f -o /home/joyvan/pygraphblas pygraphblas
+git commit -m "Generate docs"
+git push origin gh-pages
+git checkout master
