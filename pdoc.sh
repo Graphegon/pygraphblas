d1b7fe88bfd6dfdc1c065c3c610187b91d5f557d
Michel Pelletier <pelletier.michel@gmail.com>
2021-01-15 22:09:24 -0800

    add all on commit

--- pdoc.sh
+++ pdoc.sh
@@ -1,6 +1,6 @@
 BRANCH=$(git symbolic-ref --short HEAD)
 git checkout gh-pages
 ./pdoc.sh
-git commit -m "Generate docs"
+git commit -a -m "Generate docs"
 git push origin gh-pages
 git checkout $BRANCH
