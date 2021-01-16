ad9f837ed66f5a487f28106e3a2a6fc040e2f880
Michel Pelletier <pelletier.michel@gmail.com>
2021-01-15 20:25:35 -0800

    pdoc

--- pdoc.sh
+++ pdoc.sh
@@ -1,5 +1,5 @@
 git checkout gh-pages
-docker run --rm -v `pwd`:/home/joyvan/pygraphblas -it graphblas/pygraphblas-notebook:latest pdoc --html -f -o /home/joyvan/pygraphblas pygraphblas
+./pdoc.sh
 git commit -m "Generate docs"
 git push origin gh-pages
 git checkout master
