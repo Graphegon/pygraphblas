9866266e492e8e0a0e3c71244f75a40efb720e83
Michel Pelletier <pelletier.michel@gmail.com>
2021-01-16 23:07:04 -0800

    rename master to main

--- docker_build.sh
+++ docker_build.sh
@@ -2,7 +2,7 @@ if [ $# -eq 0 ]
     then
         echo "Usage: ./docker_build.sh SS_RELEASE PY_RELEASE BASE_NAME BRANCH [LOCATION PUSH]"
         echo
-        echo "Example: ./docker_build.sh v3.3.3 v3.4.0 notebook master clone push"
+        echo "Example: ./docker_build.sh v3.3.3 v3.4.0 notebook main clone push"
         exit 1
 fi
 
