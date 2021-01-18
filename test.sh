755007993a14b8a4d262e0e72c91cddeae02d1b4
Michel Pelletier <pelletier.michel@gmail.com>
2021-01-17 16:54:53 -0800

    cleanup and optional test build.

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-#SS_COMPACT=1 ./docker_build.sh v4.0.1 test minimal
+if [ "$1" = "build" ]
+then
+	SS_COMPACT=1 ./docker_build.sh v4.0.1 test minimal
+fi
 docker run --rm \
        -v `pwd`/tests:/pygraphblas/tests \
        -v `pwd`/pygraphblas:/pygraphblas/pygraphblas \
