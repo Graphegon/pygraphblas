f017c87ddd751bace97636bbdea0d207a13395a4
Michel Pelletier <pelletier.michel@gmail.com>
2021-01-19 13:34:04 -0800

    move demo out of main package.

--- notebook.sh
+++ notebook.sh
@@ -1,1 +1,3 @@
-docker run --rm --user root -e NB_UID=$(id -u) -e NB_GID=$(id -g) -p 8888:8888 -v `pwd`/pygraphblas:/home/jovyan/pygraphblas -it graphblas/pygraphblas-notebook:latest
+docker run --rm --user root -e NB_UID=$(id -u) -e NB_GID=$(id -g) -p 8888:8888 \
+	   -v `pwd`/pygraphblas:/home/jovyan/pygraphblas  -v `pwd`/demo:/home/jovyan/demo \
+	   -w /home/jovyan/demo  -it graphblas/pygraphblas-notebook:latest
