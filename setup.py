cee11173f0af307efe685872fe54cb653ff04897
Michel Pelletier <pelletier.michel@gmail.com>
2021-01-21 14:58:33 -0800

    bump setup for wheels.

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ import os
 
 setup(
     name='pygraphblas',
-    version='4.0.3-alpha1',
+    version='4.0.3a2',
     description='GraphBLAS Python bindings.',
     author='Michel Pelletier',
     packages=['pygraphblas'],
     setup_requires=["pytest-runner", "cffi>=1.0.0"],
     cffi_modules=["pygraphblas/build.py:ffibuilder"],
-    install_requires=["cffi>=1.0.0", "numpy>=1.15", "numba", "scipy"],
+    install_requires=["cffi>=1.0.0", "numpy>=1.15", "numba", "scipy", "graphviz", "matplotlib"],
     # tests_require=["pytest","pytest-cov"],
     # entry_points = {
     #     'rdf.plugins.store': [
