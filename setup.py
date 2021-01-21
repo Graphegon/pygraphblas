4f033d22a09a2d9c35faea5fffa16ac4fd1723c7
Michel Pelletier <pelletier.michel@gmail.com>
2021-01-21 14:59:04 -0800

    bump setup for wheels.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name='pygraphblas',
-    version='4.0.3a2',
+    version='4.0.3a3',
     description='GraphBLAS Python bindings.',
     author='Michel Pelletier',
     packages=['pygraphblas'],
