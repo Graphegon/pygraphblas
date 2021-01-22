34aff72e6dd29dd813e36e61bf3ab4a905538a97
Michel Pelletier <pelletier.michel@gmail.com>
2021-01-21 18:25:08 -0800

    bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name='pygraphblas',
-    version='4.0.3a4',
+    version='4.0.3',
     description='GraphBLAS Python bindings.',
     author='Michel Pelletier',
     packages=['pygraphblas'],
