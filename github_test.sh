b2645919dd4c97a88128f8011ebc99c29efd1e61
Michel Pelletier <pelletier.michel@gmail.com>
2021-02-02 21:24:35 -0800

    missing coveralls package.

--- github_test.sh
+++ github_test.sh
@@ -1,8 +1,8 @@
-SS_COMPACT=1 ./docker_build.sh v4.0.3 test minimal
+#SS_COMPACT=1 ./docker_build.sh v4.0.3 test minimal
 docker run --rm \
        -v `pwd`/pygraphblas:/pygraphblas/pygraphblas \
        -v `pwd`/demo:/pygraphblas/demo \
        -v `pwd`/docs:/docs \
        -v `pwd`/tests:/pygraphblas/tests \
        graphblas/pygraphblas-minimal:test \
-	   coverage run --branch -m pytest
+	   bash -c 'coverage run --branch -m pytest; coveralls'
