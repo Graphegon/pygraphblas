2ca21c780b652112271f008a16ec219319bddcee
Michel Pelletier <pelletier.michel@gmail.com>
2021-02-02 21:44:32 -0800

    missing coveralls package.

--- github_test.sh
+++ github_test.sh
@@ -1,5 +1,8 @@
 SS_COMPACT=1 ./docker_build.sh v4.0.3 test minimal
 docker run --rm \
+	   -e GITHUB_TOKEN=${GITHUB_TOKEN} \
+	   -e COVERALLS_FLAG_NAME=${COVERALLS_FLAG_NAME} \
+	   -e COVERALLS_PARALLEL=${COVERALLS_PARALLEL} \
        -v `pwd`/pygraphblas:/pygraphblas/pygraphblas \
        -v `pwd`/demo:/pygraphblas/demo \
        -v `pwd`/docs:/docs \
