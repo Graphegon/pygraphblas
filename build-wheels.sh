0150e7a63f2e19b836ce871b4f8e0e0277d17da6
Michel Pelletier <pelletier.michel@gmail.com>
2021-02-08 17:57:30 -0800

    build wheels script

---
+++ build-wheels.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e -u -x
+
+function repair_wheel {
+    wheel="$1"
+    if ! auditwheel show "$wheel"; then
+        echo "Skipping non-platform wheel $wheel"
+    else
+        auditwheel repair "$wheel" --plat manylinux2010_x86_64 -w /io/wheelhouse/
+    fi
+}
+
+# Compile wheels
+for PYBIN in /opt/python/cp3[678]*/bin; do
+    "${PYBIN}/pip" install -r /io/manylinux-requirements.txt
+    "${PYBIN}/pip" wheel /io/ --no-deps -w wheelhouse/
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+    repair_wheel "$whl"
+done
+
+# Install packages and test
+for PYBIN in /opt/python/cp3[678]*/bin/; do
+    "${PYBIN}/pip" install pygraphblas --no-index -f /io/wheelhouse
+    (cd "$HOME"; "${PYBIN}/pytest" pygraphblas)
+done
