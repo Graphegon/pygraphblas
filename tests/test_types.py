24de70e97472c91e6d8ad95118c04e753f4f62b7
Michel Pelletier <pelletier.michel@gmail.com>
2021-02-11 17:13:21 -0800

    add louvain to docs

--- tests/test_types.py
+++ tests/test_types.py
@@ -1,6 +1,6 @@
 import pytest
+from pygraphblas import *
 from pygraphblas import types
-
 
 def test_type_lookup_name():
     assert types.Type.gb_from_name("INT8") == types.INT8._gb_type
@@ -10,4 +10,10 @@ def test_gb_from_type():
     with pytest.raises(TypeError):
         types._gb_from_type("")
 
+def test_promotion():
+    order = []
+    for t1 in (BOOL,) + types._int_types + types._float_types:
+        for t2 in (BOOL,) + types._int_types + types._float_types:
+            order.append(types.promote(t1, t2))
+    assert order = []
     
