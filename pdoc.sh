e32a2c7fa25f0ab3380c30e094ea1cdd405bbec6
Michel Pelletier <pelletier.michel@gmail.com>
2021-08-02 20:15:35 -0400

    update pdocs

--- pdoc.sh
+++ pdoc.sh
@@ -1,7 +1,1 @@
-docker run --rm \
-	   -v `pwd`/pygraphblas:/pygraphblas/pygraphblas \
-	   -v `pwd`/demo:/pygraphblas/demo \
-	   -v `pwd`/docs:/docs \
-	   -w /pygraphblas \
-	   -it graphblas/pygraphblas-minimal:test \
-	   pdoc --html --template-dir /docs/templates -f -o /docs pygraphblas
+pdoc3 --html --template-dir docs/templates -f -o docs pygraphblas
