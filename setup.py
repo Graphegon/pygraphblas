aaa6778a53e21217fc94dc1c6c7b13e5d31acf0e
Michel Pelletier <pelletier.michel@gmail.com>
2021-09-02 13:59:20 -0400

    long description for pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup
-import os
+
+
+with open("README.md") as f:
+    long_description = f.read()
+
 
 setup(
     name="pygraphblas",
-    version="5.1.7.0",
+    version="5.1.7.1",
     description="GraphBLAS Python bindings.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Michel Pelletier",
     packages=["pygraphblas"],
     setup_requires=["pytest-runner"],
